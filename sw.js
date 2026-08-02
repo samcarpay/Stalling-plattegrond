1c66bff812955dfb18454ee38b1bbf16950baca3
samcarpay <147607011+samcarpay@users.noreply.github.com>
2026-08-02 13:08:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this string whenever you edit index.html and re-deploy, so browsers
 // pick up the new version instead of serving a stale cached copy.
-const CACHE_NAME = 'storage-sites-v12';
+const CACHE_NAME = 'storage-sites-v13';
 
 const APP_SHELL = [
   './',
