9eccb6ab900151384c6051946ac92b7c23ecc1c8
samcarpay <147607011+samcarpay@users.noreply.github.com>
2026-09-03 17:34:57 +0200

    Update CACHE_NAME to version 65

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this string whenever you edit index.html and re-deploy, so browsers
 // pick up the new version instead of serving a stale cached copy.
-const CACHE_NAME = 'storage-sites-v64';
+const CACHE_NAME = 'storage-sites-v65';
 
 const APP_SHELL = [
   './',
